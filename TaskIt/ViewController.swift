a02c3e0d2a585b21e2da8a299299ef5f69809801
Dan <danmanteufel@yahoo.com>
2014-11-03 23:50:47 -0600

    Complete through CoreData section.

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tasker.taskArray += [exampleTask1, exampleTask2, exampleTask3]
+        let taskArray = [exampleTask1, exampleTask2, exampleTask3]
+        let completedArray = [exampleTask4]
+        tasker.baseArray = [taskArray, completedArray]
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -27,8 +29,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if (tableView.indexPathForSelectedRow() != nil) {
             tableView.deselectRowAtIndexPath(tableView.indexPathForSelectedRow()!, animated: false)
         }
-        //println("viewDidAppear")
-        //tableView.reloadData()
+
+        sortAndReload()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -37,7 +39,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         case "Show Task Detail":
             let destVC = segue.destinationViewController as TaskDetailViewController
             let indexPath = tableView.indexPathForSelectedRow() //Could also go through sender
-            destVC.detailTaskModel = tasker.taskArray[indexPath!.row]
+            destVC.detailTaskModel = tasker.baseArray[indexPath!.section][indexPath!.row]
             destVC.delegate = self
         case "Show Add Task":
             let destVC = segue.destinationViewController as AddTaskViewController
@@ -47,15 +49,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+    //MARK: Helper Functions
+    func sortAndReload() {
+        
+        //THIS IS EQUIVALENT TO THE CLOSURE BELOW
+        //        func sortByDate (taskOne: TaskModel, taskTwo: TaskModel) -> Bool {
+        //            return taskOne.date.timeIntervalSince1970 < taskTwo.date.timeIntervalSince1970
+        //        }
+        //
+        //        tasker.taskArray = tasker.taskArray.sorted(sortByDate)
+        
+        for (index, _) in enumerate (tasker.baseArray) {
+            tasker.baseArray[index] = tasker.baseArray[index].sorted{
+                (taskOne: TaskModel, taskTwo: TaskModel) -> Bool in
+                return taskOne.date.timeIntervalSince1970 < taskTwo.date.timeIntervalSince1970
+            }
+        }
+        tableView.reloadData()
+    }
+    
     //MARK: UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tasker.baseArray.count
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasker.taskArray.count
+        return tasker.baseArray[section].count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("My Cell") as TaskCell
+        //cell.editingAccessoryView = TaskCell.setCompletionAccessory()
+        //cell.editingAccessoryType = UITableViewCellAccessoryType.Checkmark
         
-        let task = tasker.taskArray[indexPath.row]
+        let task = tasker.baseArray[indexPath.section][indexPath.row]
         
         cell.taskLabel.text = task.task
         cell.subtaskLabel.text = task.subtask
@@ -71,16 +98,43 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "To Do"
+        }
+        else if section == 1 {
+            return "Completed"
+        }
+        else {
+            return "?"
+        }
+    }
+    
+    func tableView(tableView: UITableView,
+                   commitEditingStyle editingStyle: UITableViewCellEditingStyle,
+                   forRowAtIndexPath indexPath: NSIndexPath) {
+        var task = tasker.baseArray[indexPath.section][indexPath.row]
+        task.completed = !task.completed
+        tasker.baseArray[indexPath.section].removeAtIndex(indexPath.row)
+        tasker.baseArray[indexPath.section == 0 ? 1 : 0] += [task]
+        sortAndReload()
+    }
+    
     //MARK: TaskDetailViewControllerDelegate
     func updateTask(task: TaskModel) {
         let indexPath = tableView.indexPathForSelectedRow()!
-        tasker.taskArray[indexPath.row] = task
-        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .None)
+        tasker.baseArray[indexPath.section][indexPath.row] = task
+        //tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .None)
+        //Only because we reload all after sorting in viewDidAppear
     }
     
     //MARK: AddTaskViewControllerDelegate
     func addTask(task: TaskModel) {
-        tasker.taskArray += [task]
+        tasker.baseArray[0] += [task]
     }
 }
 
@@ -106,7 +160,8 @@ class TaskDetailViewController: UIViewController, UITextFieldDelegate {
     @IBAction func saveButtonPressed(sender: UIBarButtonItem) {
         var task = TaskModel(task: taskTextField.text,
                              subtask: subtaskTextField.text,
-                             date: dueDatePicker.date)
+                             date: dueDatePicker.date,
+                             completed: false)
         delegate?.updateTask(task)
         navigationController?.popViewControllerAnimated(true)
     }
@@ -139,10 +194,12 @@ class AddTaskViewController: UIViewController, UITextFieldDelegate {
     @IBAction func cancelButtonPressed(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
     @IBAction func addButtonPressed(sender: UIButton) {
         var task = TaskModel(task: taskTextField.text,
                              subtask: subtastTextField.text,
-                             date: dueDatePicker.date)
+                             date: dueDatePicker.date,
+                             completed: false)
         delegate?.addTask(task)
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -161,6 +218,12 @@ class TaskCell: UITableViewCell {
     @IBOutlet weak var subtaskLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+//    class func setCompletionAccessory() -> UIView {
+//        var completionBox = UIView(frame: CGRect(x: 0, y: 0,
+//                                                 width: 75, height: 75))
+//        completionBox.tintColor = .greenColor()
+//        return completionBox
+//    }
 }
 
 //MARK: - Model
@@ -169,13 +232,14 @@ class TaskCell: UITableViewCell {
 
 //MARK: Structs
 struct TaskIt {
-    var taskArray: [TaskModel] = []
+    var baseArray: [[TaskModel]] = [[]]
 }
 
 struct TaskModel {
     var task = ""
     var subtask = ""
     var date = NSDate(timeIntervalSinceNow: 0)
+    var completed = false
 }
 
 //MARK: Classes
@@ -199,10 +263,17 @@ class Date {
 //MARK: Example Data
 let exampleTask1 = TaskModel(task: "Study French",
                              subtask: "Verbs",
-                             date: Date.fromYear(2014, month: 11, day: 3))
+                             date: Date.fromYear(2014, month: 11, day: 3),
+                             completed: false)
 let exampleTask2 = TaskModel(task: "Eat Dinner",
                              subtask: "Burgers",
-                             date: Date.fromYear(2014, month: 11, day: 3))
+                             date: Date.fromYear(2014, month: 11, day: 5),
+                             completed: false)
 let exampleTask3 = TaskModel(task : "Gym",
                              subtask: "Leg Day",
-                             date: Date.fromYear(2014, month: 11, day: 4))
+                             date: Date.fromYear(2014, month: 11, day: 4),
+                             completed: false)
+let exampleTask4 = TaskModel(task: "Code",
+                             subtask: "Task Project",
+                             date: Date.fromYear(2014, month: 11, day: 1),
+                             completed: true)
